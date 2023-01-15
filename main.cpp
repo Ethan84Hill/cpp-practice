8c17d8653fdc3a1d9077f1e2762f32fde06d5c6d
Ethan <ethan84hill@gmail.com>
2023-01-15 13:14:46 -0500

    took a bunch of notes

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+// using namespace std;
+
+int main() {
+    // int file_size = 0;
+    // int counter = 0;
+    // double sales = 9.99;
+    // std::cout << file_size;
+    // return 0;
+
+
+    // int a = 1;
+    // using int in cpp is like using let in javascript
+    // int b = 2;
+    // int temp = a;
+    // a = b;
+    // b = temp;
+    // this goes in a sequence so you set temp to a or 1 and then a to b so it makes it 2 and then once you set b to temp is makes b 1
+    // so when you do cout or character out << a it says 2 and if you were to use b it would be 1 therefore swapping them
+    // cout << a;
+
+    // const double pi = 3.14;
+    // by using the const it makes it so that the variable pi doesn't change so it will always be 3.14 and will throw an error if you try to change it
+    // using double allows you to use decimals for the value given to the variable
+
+    // int file_size; // Snake Case using lowercase letters
+    // int FileSize; // Pascal Case uses uppercase for the first letters of the words
+    // int fileSize; // Camel Case (I know what this is)
+    // int iFileSize; // Hungarian Notation (old and not relevant)
+
+    // int x = 10;
+    // int y = 3;
+    // int z = x + y;
+    // std::cout << z;
+    // by doing the above it will out put 13 when you run the program because it calls z which is x + y
+    // if you want to do division and get a decimal to show you have to add a double instead of int infront of the z so it would be double z = x / y;
+    // you also need to use a double for one of the variables aswell so you'd need to change x to  double x = 10; instead of
+    // int x = 10
+    // you can use x++ for incrementing or x-- for decrementing instead of doing x - 1 or x + 1
+    // if you were to do std::cout << "z = " << z; it will use the string and chain it to z so it will say z = to whatever the value of z is when you run it
+    // using std::endl; will start a new line so if you do
+    // std:cout << "z = " << z << std::endl; and then a another std:cout underthat they will be on seperate lines when you run the program
+    // or you can use on the next line of code << instead so on the next line you could use << "y = " << y; and remove
+    // the ; from the std::endl;
+    // by using.... using namespace std; like you see above it allows you to skip using std:: before the cout or end
+    // so the next time you use it you can use cout or endl anywhere and not have to use std::cout or endl and just use cout or endl instead
+    return 0;
+}
